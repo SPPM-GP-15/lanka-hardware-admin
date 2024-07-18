e27d205d07b2ff164a4a1e375ee7378ab665261d
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-07-18 19:55:47 +0530

    Small update

--- src/routes/initial/Initial.jsx
+++ src/routes/initial/Initial.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Initial = () => {
   return (
     <div>
-      <h1>Initial Component</h1>
+      <h1 className="text-3xl font-bold underline">Initial Component</h1>
+      <Sidebar />
       <Outlet />
     </div>
   );
