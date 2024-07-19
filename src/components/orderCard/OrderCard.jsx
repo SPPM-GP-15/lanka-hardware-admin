cd625463607a6695f8a938ae3961c74191ef8a92
asjedh-dilimatrix <Mohamed.Asjedh@dilimatrix.com>
2024-07-20 01:20:15 +0530

    Designed orders page

---
+++ src/components/orderCard/OrderCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { IoCloudDownloadSharp } from "react-icons/io5";
+import { MdFileDownloadDone } from "react-icons/md";
+import Stepper from "../stepper/Stepper";
+
+const OrderCard = () => {
+  return (
+    <div>
+      <div>
+        <div className="border border-gray-300 rounded-lg shadow-md p-4 mb-4 relative">
+          <div className="flex justify-between items-start mb-2">
+            <div>
+              <div className="text-lg font-semibold">Order #0001</div>
+              <div className="text-gray-600">July 27, 2002 at 09:44 AM</div>
+            </div>
+            <div className="flex space-x-2">
+              <div className="flex items-center px-3 py-1 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-100">
+                <IoCloudDownloadSharp className="mr-1" />
+                <span>Download</span>
+              </div>
+
+              <div className="flex items-center px-3 py-1 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-100">
+                <MdFileDownloadDone className="mr-1" />
+                <span>Completed</span>
+              </div>
+            </div>
+          </div>
+          <Stepper />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCard;
--- src/routes/orders/Orders.jsx
+++ src/routes/orders/Orders.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import OrderCard from "../../components/orderCard/OrderCard";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div>
+      <div className="flex gap-3 mb-4">
+        <div className="px-4 py-2 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-100">
+          Completed
+        </div>
+        <div className="px-4 py-2 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-100">
+          On Progress
+        </div>
+      </div>
+      <OrderCard />
+      <OrderCard />
+      <OrderCard />
+      <OrderCard />
+    </div>
+  );
 };
 
 export default Orders;
