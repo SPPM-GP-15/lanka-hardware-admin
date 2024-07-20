52fb01cf01c83b13fedf1a95cec30c05b465d710
asjedh-dilimatrix <Mohamed.Asjedh@dilimatrix.com>
2024-07-20 12:03:51 +0530

    Desined orders page

--- src/routes/orders/Orders.jsx
+++ src/routes/orders/Orders.jsx
@@ -5,10 +5,10 @@ const Orders = () => {
   return (
     <div>
       <div className="flex gap-3 mb-4">
-        <div className="px-4 py-2 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-100">
+        <div className="px-4 py-2 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-200">
           Completed
         </div>
-        <div className="px-4 py-2 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-100">
+        <div className="px-4 py-2 border border-gray-900 rounded-lg cursor-pointer hover:bg-gray-200">
           On Progress
         </div>
       </div>
