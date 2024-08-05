7a5fcffd356f34039f2798fc2be5370dc61751c9
Krishantha Bandara <142531941+KrishanthaBSomarathna@users.noreply.github.com>
2024-08-05 16:14:02 +0530

    Update UserCard.jsx

--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -30,20 +30,7 @@ function UserCard({ user, onRemove, onBlock }) {
           </div>
         </div>
 
-        <div className="flex justify-end space-x-4 mt-4">
-          <button
-            onClick={() => onRemove(user.id)}
-            className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
-          >
-            Remove
-          </button>
-          <button
-            onClick={() => onBlock(user.id)}
-            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          >
-            Block
-          </button>
-        </div>
+        
       </div>
     </div>
   );
